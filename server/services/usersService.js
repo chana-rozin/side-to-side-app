c5f4bfc207c8b5e06062b99902c179cb849790f5
chana-rozin <c0556787069@gmail.com>
2024-04-02 20:33:18 +0300

    delete user

--- server/services/usersService.js
+++ server/services/usersService.js
@@ -23,14 +23,34 @@ export class UsersService {
     }
 
     async deleteUser(id){
+        const user = await executeQuery(getByIdQuery("users","id"), [id]);
         const queryUser = softDeleteQuery('users', 'id');
         const result = await executeQuery(queryUser, [id]);
-        
+        const queryTodo = deleteQuery("todos", "userId");
+        await executeQuery(queryTodo,[id]);
+        const userAlbumsQuery = getByIdQuery("albums", "userId");
+        const userAlbums = await executeQuery(userAlbumsQuery,[id]);
+        userAlbums.forEach(async el=>{
+            const queryPhoto = deleteQuery("photos", "albumId");
+            await executeQuery(queryPhoto,[el.id]);
+        })
+        const queryAlbum = deleteQuery("albums", "userId");
+        await executeQuery(queryAlbum,[id]);
+        const userPostQuery = getByIdQuery("posts", "userId");
+        const userPosts = await executeQuery(userPostQuery,[id]);
+        userPosts.forEach(async el=>{
+            const queryComment = deleteQuery("comments", "postId");
+            await executeQuery(queryComment,[el.id]);
+        })
+        const queryPost = deleteQuery("posts", "userId");
+        await executeQuery(queryPost,[id]);
+        const queryComment = deleteQuery("comments", "email");
+        await executeQuery(queryPost,[user.email]);
         return result;
     }
 
     async updateUser(userItem, id){
-        const queryUser = updateQuery('users', 'id', usersColumns,, true);
+        const queryUser = updateQuery('users', 'id', usersColumns, true);
         const result =  await executeQuery(queryUser, [userItem.name, userItem.username, userItem.email, userItem.address, userItem.phone, id]);
         return result;
     }
