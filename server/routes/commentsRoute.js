389a24e1045883164be10bb6354c46b2c55e268e
chana-rozin <c0556787069@gmail.com>
2024-04-02 23:57:13 +0300

    validations

--- server/routes/commentsRoute.js
+++ server/routes/commentsRoute.js
@@ -1,15 +1,17 @@
 
 import express from "express";
 import { CommentsController } from '../controllers/commentsController.js'
+import { validateCommentData, validate } from "../middleware/validationMiddleware.js";
+
 const commentsRouter = express.Router();
 
 const commentscontroller = new CommentsController()
 
 commentsRouter.get("/:id", commentscontroller.getCommentById)
 commentsRouter.get("/", commentscontroller.getComment)
-commentsRouter.post("/", commentscontroller.addComment)
+commentsRouter.post("/",validate(validateCommentData), commentscontroller.addComment)
 commentsRouter.delete("/:id", commentscontroller.deleteComment)
-commentsRouter.put("/:id", commentscontroller.updateComment)
+commentsRouter.put("/:id",validate(validateCommentData), commentscontroller.updateComment)
 
 export default
     commentsRouter
