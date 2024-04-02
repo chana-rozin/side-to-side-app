aa74434d9d846dcab6470bd8222cb97547d75fbe
chana-rozin <c0556787069@gmail.com>
2024-04-02 23:31:22 +0300

    start validation

---
+++ server/middleware/validationMiddleware.js
@@ -0,0 +1,29 @@
+import { validationResult } from 'express-validator';
+
+const validateUserData = [
+    body('name').isLength({ max: 45 }).withMessage('Name must be at most 45 characters'),
+    body('username').isLength({ max: 64 }).withMessage('Username must be at most 64 characters'),
+    body('email').isEmail().withMessage('Invalid email'),
+    body('address').isObject().withMessage('Address must be an object'),
+    body('address.street').exists().withMessage('Street is required'),
+    body('address.city').exists().withMessage('City is required'),
+    body('address.zipcode').exists().withMessage('Zipcode is required'),
+    body('phone').matches(/^(?:0(?!([57]))(?:[23489] ?(?:\d ?){7}|\d ?(?:\d ?){7})|0([57])\d ?(?:\d ?){7}|(?:\+972|972|0)(?:\d ?(?:\d ?){7}|\d ?(?:\d ?){8}))$/).withMessage('Invalid phone number')
+];
+
+const validatePostData = [
+    
+];
+
+function validate(validationRules) {
+    return (req, res, next) => {
+        validationRules(req, res, next);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    };
+}
+
+export { validateUserData, validate };
