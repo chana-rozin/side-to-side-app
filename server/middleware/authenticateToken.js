e3642808ed3e56759d20c397d9d9ca70d2e012fa
chana-rozin <c0556787069@gmail.com>
2024-04-03 18:45:52 +0300

    some changes

--- server/middleware/authenticateToken.js
+++ server/middleware/authenticateToken.js
@@ -1,12 +1,15 @@
 import jwt from 'jsonwebtoken';
 
 const authenticateToken = (req, res, next) => {
+    console.log("start authenticateToken")
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[0];
     if (token == null) return res.sendStatus(401);
-
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
+        if (err){ 
+            console.log("fail authenticateToken");
+            return res.sendStatus(403);}
+        console.log("pass authenticateToken", user)
         req.user = user;
         next();
     });
