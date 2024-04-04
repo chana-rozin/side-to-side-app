bdb3d1f982858b05b1e04bc4db978896af709f02
naamashvalb <naamah.shv@gmail.com>
2024-04-04 23:41:29 +0200

    fix

--- server/services/db.js
+++ server/services/db.js
@@ -1,20 +1,13 @@
 import * as mysql from 'mysql2/promise';
 import 'dotenv/config';
 
-let connection = null;
-
 async function createConnection() {
-  if (connection) {
-    return connection;
-  } else {
-    connection =  await mysql.createConnection({
+    return mysql.createConnection({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
     });
-    return connection;
-}
 }
 
 export { createConnection };
