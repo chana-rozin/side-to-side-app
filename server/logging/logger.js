6d233f957a66d99070f9c83d5d907ecf87ec1efb
naamashvalb <naamah.shv@gmail.com>
2024-04-04 22:58:06 +0200

    fix logger route

--- server/logging/logger.js
+++ server/logging/logger.js
@@ -1,15 +1,14 @@
-import executeQuery from '../services/db'
-
-async function logAction(action, userId = null) {
-  const queryString = 'INSERT INTO logs (action, user_id) VALUES (?, ?)';
-  const values = [action, userId];
-
+async function logAction(connection, action, userId = null) {
+    const queryString = 'INSERT INTO logs (action, user_id) VALUES (?, ?)';
+    const values = [action, userId];
+    
     try {
-        await executeQuery(queryString, values);
-        console.log('Action logged successfully.');
+      await connection.execute(queryString, values);
+      console.log('Action logged successfully.');
     } catch (error) {
-        console.error('Error logging action:', error);
-}
-}
-
-export default logAction;
+      console.error('Error logging action:', error);
+    }
+  }
+  
+  export { logAction };
+  
---
+++ server/services/executeQuery.js
@@ -0,0 +1,27 @@
+import { createConnection } from './db.js';
+import { logAction } from '../logging/logger.js';
+
+
+async function executeQuery(query, params, userId) {
+  let results;
+  console.log(params);
+  const connection = await createConnection();
+  
+  try {
+    [results] = await connection.execute(query, params);
+    console.log("query executed successfully:", results);
+    if (userId) {
+      await logAction(connection, `Query executed: ${query}`, userId);
+    }
+  } catch (err) {
+    console.log("error executing query:", err);
+    throw `error executing query: ${err}`
+  } finally {
+    await connection.end();
+    console.log("connection closed");
+  }
+  
+  return results;
+}
+
+export { executeQuery };
